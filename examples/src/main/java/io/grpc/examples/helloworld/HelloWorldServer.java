a42415023bbe10124a6bf6310b4288cf6564c27a
Ben Scarlato <ben.scarlato@boringcompany.com>
2020-09-04 22:19:50 -0700

    Update HelloWorldServer.java

--- examples/src/main/java/io/grpc/examples/helloworld/HelloWorldServer.java
+++ examples/src/main/java/io/grpc/examples/helloworld/HelloWorldServer.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2015 The gRPC Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Neuralink image server.
  */
 
 package io.grpc.examples.helloworld;
@@ -38,9 +26,9 @@ public class HelloWorldServer {
   private void start() throws IOException {
     /* The port on which the server should run */
     int port = 50051;
-    // int port = 50071;
     server = ServerBuilder.forPort(port)
-        .addService(new GreeterImpl())
+        // .addService(new GreeterImpl())
+        .addService(new NLImageServiceImpl)
         .build()
         .start();
     logger.info("Server started, listening on " + port);
