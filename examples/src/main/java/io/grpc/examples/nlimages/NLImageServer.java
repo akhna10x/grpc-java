dcc26aff01cc799f1fa5ef369d93ad0b7b7abf3c
Ben Scarlato <ben.scarlato@boringcompany.com>
2020-09-06 12:19:53 -0700

    Update NLImageServer.java

--- examples/src/main/java/io/grpc/examples/nlimages/NLImageServer.java
+++ examples/src/main/java/io/grpc/examples/nlimages/NLImageServer.java
@@ -24,6 +24,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.io.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.DataBufferByte;
@@ -95,6 +96,25 @@ public class NLImageServer {
   // NLImageService
   static class NLImageServiceImpl extends NLImageServiceGrpc.NLImageServiceImplBase  {
     
+    public static Image getImageFromArray(int[] pixels, int width, int height) {
+      BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+      WritableRaster raster = (WritableRaster) image.getData();
+      raster.setPixels(0,0,width,height,pixels);
+      return image;
+    }
+
+    public ImageIcon createImage(byte[] b){
+      // byte[] b = {-119, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82,
+      //    0, 0, 0, 15, 0, 0, 0, 15, 8, 6, 0, 0, 0, 59, -42, -107,
+      //    74, 0, 0, 0, 64, 73, 68, 65, 84, 120, -38, 99, 96, -64, 14, -2,
+      //    99, -63, 68, 1, 100, -59, -1, -79, -120, 17, -44, -8, 31, -121, 28, 81,
+      //    26, -1, -29, 113, 13, 78, -51, 100, -125, -1, -108, 24, 64, 86, -24, -30,
+      //    11, 101, -6, -37, 76, -106, -97, 25, 104, 17, 96, -76, 77, 97, 20, -89,
+      //    109, -110, 114, 21, 0, -82, -127, 56, -56, 56, 76, -17, -42, 0, 0, 0,
+      //    0, 73, 69, 78, 68, -82, 66, 96, -126};
+      return new ImageIcon(b);
+   }
+
       @Override
       public void rotateImage(NLImageRotateRequest req, StreamObserver<NLImage> responseObserver) {
         logger.info("Runng RotateImage() impl...."); 
@@ -107,44 +127,35 @@ public class NLImageServer {
         byte[] rotatedImg = new byte[width * height];
         ByteString reqImgBytes = reqImg.getData();
         logger.info("__ reqImgBytes.size(): " + reqImgBytes.size());
+        byte[] bytes = reqImgBytes.toByteArray();
+        ImageIcon icon = createImage(bytes);
+        displayResponse(icon);
+        logger.info("displayResponse");
 
         /**
          * Reads bytes back to img object.
          */
-        try {
-          byte[] bytes = reqImgBytes.toByteArray();
+        // try {
+        //   byte[] bytes = reqImgBytes.toByteArray();
 
-          ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
-          // Iterator<?> readers = ImageIO.getImageReadersByFormatName("png");
-          Iterator<?> readers = ImageIO.getImageReadersByFormatName("bmp");
-          ImageReader reader = (ImageReader) readers.next();
-          Object source = bis; 
-          ImageInputStream iis = ImageIO.createImageInputStream(source); 
-          reader.setInput(iis, true);
-          ImageReadParam param = reader.getDefaultReadParam();
-          java.awt.Image image = reader.read(0, param);
-          BufferedImage bufferedImage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_RGB);
-          //bufferedImage is the RenderedImage to be written
-          Graphics2D g2 = bufferedImage.createGraphics();
-          g2.drawImage(image, null, null);
-          System.out.println("g2.drawImage() img drawn...");
-          displayResponse(bufferedImage);
-        } catch(IOException e) {
-          logger.info("IOException: " + e.getMessage());
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
+        //   ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        //   // Iterator<?> readers = ImageIO.getImageReadersByFormatName("png");
+        //   Iterator<?> readers = ImageIO.getImageReadersByFormatName("bmp");
+        //   ImageReader reader = (ImageReader) readers.next();
+        //   Object source = bis; 
+        //   ImageInputStream iis = ImageIO.createImageInputStream(source); 
+        //   reader.setInput(iis, true);
+        //   ImageReadParam param = reader.getDefaultReadParam();
+        //   java.awt.Image image = reader.read(0, param);
+        //   BufferedImage bufferedImage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_RGB);
+        //   //bufferedImage is the RenderedImage to be written
+        //   Graphics2D g2 = bufferedImage.createGraphics();
+        //   g2.drawImage(image, null, null);
+        //   System.out.println("g2.drawImage() img drawn...");
+        //   displayResponse(bufferedImage);
+        // } catch(IOException e) {
+        //   logger.info("IOException: " + e.getMessage());
+        // }
 
         /**
          * Sends reply back.
@@ -157,6 +168,15 @@ public class NLImageServer {
             // TODO: set data
         responseObserver.onNext(reply);
         responseObserver.onCompleted();
+      }
+
+      private void displayResponse(ImageIcon icon) {
+        // TODO: remove this method
+        JFrame frame = new JFrame();
+        JLabel label = new JLabel(icon);
+        frame.add(label);
+        frame.pack();
+        frame.setVisible(true);
       }
 
       private void displayResponse(BufferedImage bufferedImage) {
