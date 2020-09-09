5b5e927b739e543cb66f2886677cd145f27a0628
Ben Scarlato <ben.scarlato@boringcompany.com>
2020-09-09 08:39:46 -0700

    Update NLImageServer.java

--- examples/src/main/java/com/neuralink/interviewing/nlimages/NLImageServer.java
+++ examples/src/main/java/com/neuralink/interviewing/nlimages/NLImageServer.java
@@ -81,143 +81,39 @@ public class NLImageServer {
 		}
 	}
 
-	// NLImageService
-	static class NLImageServiceImpl extends NLImageServiceGrpc.NLImageServiceImplBase  {n
-		public static BufferedImage convertToBufferedImage(
-				java.awt.Image image) {
-			BufferedImage newImage = new BufferedImage(
-					image.getWidth(null), image.getHeight(null),
-					BufferedImage.TYPE_INT_ARGB);
-			Graphics2D g = newImage.createGraphics();
-			g.drawImage(image, 0, 0, null);
-			g.dispose();
-			return newImage;
-		}
-
-		public ImageIcon createImage(byte[] b, int width, 
-				int height, 
-				NLImageRotateRequest.Rotation rotation) {
-			ImageIcon icon = new ImageIcon(b);
-			java.awt.Image rawImage = icon.getImage();
-			BufferedImage image = convertToBufferedImage(rawImage);
-			icon.setImage(createFlipped(image));
-			if (rotation == NLImageRotateRequest.Rotation.NONE) { 
-				icon.setImage(rotate(image,0.0));
-			} else if (rotation == NLImageRotateRequest.Rotation.NINETY_DEG) {
-				icon.setImage(rotate(image, 90.0));
-			} else if (rotation == NLImageRotateRequest.Rotation.ONE_EIGHTY_DEG) {
-				icon.setImage(rotate(image, 180.0));
-			} else if (rotation == NLImageRotateRequest.Rotation.TWO_SEVENTY_DEG) {
-				icon.setImage(rotate(image, 270.0));
-			}
-
-			return icon;
-		}
-
-		public ImageIcon createWatermarkImage(byte[] b, int width, int height) {
-			ImageIcon icon = new ImageIcon(b);
-			java.awt.Image rawImage = icon.getImage();
-			BufferedImage image = convertToBufferedImage(rawImage);
-			icon.setImage(createWatermarked(image));
-			return icon;
-		}
-
-
-		public static GraphicsConfiguration getDefaultConfiguration() {
-			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-			GraphicsDevice gd = ge.getDefaultScreenDevice();
-			return gd.getDefaultConfiguration();
-		}
-
-		public static BufferedImage rotate(BufferedImage image, double angle) {
-			int w = image.getWidth(), h = image.getHeight();
-			GraphicsConfiguration gc = getDefaultConfiguration();
-			BufferedImage result = gc.createCompatibleImage(w, h);
-			Graphics2D g = result.createGraphics();
-			g.rotate(Math.toRadians(angle), w / 2, h / 2);
-			g.drawRenderedImage(image, null);
-			g.dispose();
-			return result;
-		}
-
-		static BufferedImage addImageWatermark(BufferedImage sourceImage) {
-			int w = sourceImage.getWidth();
-			int h = sourceImage.getHeight();
-			GraphicsConfiguration gc = getDefaultConfiguration();
-			BufferedImage result = gc.createCompatibleImage(w, h);
-			try {
-				String filename = "watermark.png";
-				BufferedImage watermarkImage = ImageIO.read(new File("watermark.png"));
-
-				Graphics2D g = result.createGraphics();
-				Graphics2D g2d = (Graphics2D) sourceImage.getGraphics();
-				AlphaComposite alphaChannel = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.3f);
-				g2d.setComposite(alphaChannel);
-				int topLeftX = (sourceImage.getWidth() - watermarkImage.getWidth()) / 2;
-				int topLeftY = (sourceImage.getHeight() - watermarkImage.getHeight()) / 2;
-				g2d.drawImage(watermarkImage, topLeftX, topLeftY, null);
-				System.out.println("The image watermark is added to the image.");
-				g.dispose();
-				return sourceImage;
-
-			} catch (IOException ex) {
-				System.err.println(ex);
-			}
-			return result;
-		}
-
-		private static BufferedImage createFlipped(BufferedImage image){
-			return rotate(image, 180.0);
-		}
-		private static BufferedImage createWatermarked(BufferedImage image){
-			return addImageWatermark(image);
-		}
+	/** 
+	 * NLImageService implementation.
+	 */
+	static class NLImageServiceImpl extends NLImageServiceGrpc.NLImageServiceImplBase {
 
 		@Override
 		public void rotateImage(NLImageRotateRequest req, StreamObserver<NLImage> responseObserver) {
 			NLImage reqImg = req.getImage();
 			int height = reqImg.getHeight();
 			int width = reqImg.getWidth();
-			byte[] rotatedImg = new byte[width * height];
 			ByteString reqImgBytes = reqImg.getData();
 			byte[] bytes = reqImgBytes.toByteArray();
 			ImageIcon icon = 
-					createImage(bytes, width, height, req.getRotation());
+					createRotatedImage(bytes, width, height, req.isColor(), req.getRotation());
 			displayResponse(icon);
 			try {
-				ByteArrayOutputStream baos = new ByteArrayOutputStream ();
-				ImageOutputStream stream = new MemoryCacheImageOutputStream(baos);
-				ImageIO.write((
-						(BufferedImage) icon.getImage()), "png", stream);
-				stream.close();
-				baos.toByteArray();
-
-
 				int newWidth = width;
-        int newHeight = height;
-        if (rotation == NLImageRotateRequest.Rotation.NINETY_DEG ||
-            rotation == NLImageRotateRequest.Rotation.TWO_SEVENTY_DEG) {
-              newWidth = height;
-              newHeight = width;
-        }
+				int newHeight = height;
+				if (rotation == NLImageRotateRequest.Rotation.NINETY_DEG ||
+						rotation == NLImageRotateRequest.Rotation.TWO_SEVENTY_DEG) {
+					newWidth = height;
+					newHeight = width;
+				}
 				NLImage reply = NLImage.newBuilder()
 						.setWidth(newWidth)
 						.setHeight(newHeight)
-						.setData(ByteString.copyFrom(baos.toByteArray()))
+            .setData(imgToByteString(icon))
 						.build();
 				responseObserver.onNext(reply);
 				responseObserver.onCompleted();
 			} catch(IOException e) {
 				System.err.println("IOExceotion: " + e.getMessage());
 			}
-		}
-
-		private void displayResponse(ImageIcon icon) {
-			JFrame frame = new JFrame();
-			JLabel label = new JLabel(icon);
-			frame.add(label);
-			frame.pack();
-			frame.setVisible(true);
 		}
 
 		@Override 
@@ -231,18 +127,10 @@ public class NLImageServer {
 			ImageIcon icon = createWatermarkImage(bytes, width, height);
 			displayResponse(icon);
 			try {
-				ByteArrayOutputStream baos = new ByteArrayOutputStream();
-				ImageOutputStream stream = new MemoryCacheImageOutputStream(baos);
-				ImageIO.write((
-						(BufferedImage) icon.getImage()), "png", stream);
-				stream.close();
-				baos.toByteArray();
-
-
 				NLImage replyImg = NLImage.newBuilder()
 						.setWidth(width)
 						.setHeight(height)
-						.setData(ByteString.copyFrom(baos.toByteArray()))
+						.setData(imgToByteString(icon))
 						.build();
 				NLCustomImageEndpointResponse reply = NLCustomImageEndpointResponse.newBuilder()
 						.setImage(replyImg)
@@ -272,9 +160,17 @@ public class NLImageServer {
 		g.drawImage(image, 0, 0, null);
 		g.dispose();
 		return newImage;
-	}
+  }
+  
+  private ByteString imgToByteString(ImageIcon icon) {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    ImageOutputStream stream = new MemoryCacheImageOutputStream(baos);
+    ImageIO.write((
+        (BufferedImage) icon.getImage()), "png", stream);
+    return ByteString.copyFrom(baos.toByteArray());
+  }
 
-	private ImageIcon createImage(byte[] b, int width, 
+	private ImageIcon createRotatedImage(byte[] b, int width, 
 			int height, boolean color, 
 			NLImageRotateRequest.Rotation rotation) {
 		ImageIcon icon = new ImageIcon(b);
@@ -282,7 +178,7 @@ public class NLImageServer {
 		BufferedImage image = convertToBufferedImage(rawImage, color);
 		icon.setImage(createFlipped(image));
 		if (rotation == NLImageRotateRequest.Rotation.NONE) { 
-			icon.setImage(rotate(image,0.0));
+			icon.setImage(rotate(image, 0.0));
 		} else if (rotation == NLImageRotateRequest.Rotation.NINETY_DEG) {
 			icon.setImage(rotate(image, 90.0));
 		} else if (rotation == NLImageRotateRequest.Rotation.ONE_EIGHTY_DEG) {
@@ -302,14 +198,13 @@ public class NLImageServer {
 		return icon;
 	}
 
-
-	public static GraphicsConfiguration getDefaultConfiguration() {
+	private  static GraphicsConfiguration getDefaultConfiguration() {
 		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		GraphicsDevice gd = ge.getDefaultScreenDevice();
 		return gd.getDefaultConfiguration();
 	}
 
-	public static BufferedImage rotate(BufferedImage image, double angle) {
+	public BufferedImage rotate(BufferedImage image, double angle) {
 		int w = image.getWidth(), h = image.getHeight();
 		GraphicsConfiguration gc = getDefaultConfiguration();
 		BufferedImage result = gc.createCompatibleImage(w, h);
@@ -320,13 +215,12 @@ public class NLImageServer {
 		return result;
 	}
 
-	public static BufferedImage addImageWatermark(BufferedImage sourceImage) {
+	private BufferedImage addImageWatermark(BufferedImage sourceImage) {
 		int w = sourceImage.getWidth();
 		int h = sourceImage.getHeight();
 		GraphicsConfiguration gc = getDefaultConfiguration();
 		BufferedImage result = gc.createCompatibleImage(w, h);
 		try {
-			String filename = "watermark.png";
 			BufferedImage watermarkImage = ImageIO.read(new File("watermark.png"));
 
 			Graphics2D g = result.createGraphics();
@@ -336,25 +230,31 @@ public class NLImageServer {
 			int topLeftX = (sourceImage.getWidth() - watermarkImage.getWidth()) / 2;
 			int topLeftY = (sourceImage.getHeight() - watermarkImage.getHeight()) / 2;
 			g2d.drawImage(watermarkImage, topLeftX, topLeftY, null);
-			System.out.println("The image watermark is added to the image.");
 			g.dispose();
 			return sourceImage;
-
 		} catch (IOException ex) {
 			System.err.println(ex);
 		}
 		return result;
 	}
 
-	private static BufferedImage createFlipped(BufferedImage image){
+	private void displayResponse(ImageIcon icon) {
+		JFrame frame = new JFrame();
+		JLabel label = new JLabel(icon);
+		frame.add(label);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	private BufferedImage createFlipped(BufferedImage image){
 		return rotate(image, 180.0);
 	}
-	private static BufferedImage createWatermarked(BufferedImage image){
+	private BufferedImage createWatermarked(BufferedImage image){
 		return addImageWatermark(image);
 	}
 
 	/**
-	 * Launches the server from the command line.
+	 * Launches the server.
 	 */
 	public static void main(String[] args) throws IOException, InterruptedException {
 		final NLImageServer server = new NLImageServer();
