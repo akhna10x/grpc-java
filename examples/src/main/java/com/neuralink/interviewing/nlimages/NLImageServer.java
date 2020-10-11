201210038e6cbb57534cb7b4133bb0187e014f8d
Ben Scarlato <ben.scarlato@boringcompany.com>
2020-10-10 20:41:11 -0700

    Update NLImageServer.java

--- examples/src/main/java/com/neuralink/interviewing/nlimages/NLImageServer.java
+++ examples/src/main/java/com/neuralink/interviewing/nlimages/NLImageServer.java
@@ -405,21 +405,22 @@ public class NLImageServer {
 		return ByteString.copyFrom(baos.toByteArray());
 	}
 
-	private static byte[] createGrayscaleImage(byte[] b, int width, int height, boolean color) {
-		byte[] ret = new byte[0];
+	private static byte[] createGrayscaleImage(byte[] bytes, int width, int height, boolean color) {
+		byte[] ret = new byte[bytes.length/3];
 		// TODO: implement
 		// New grayscale image = ( (0.3 * R) + (0.59 * G) + (0.11 * B) ).
 		final double rFactor = 0.3;
 		final double gFactor = 0.59;
 		final double bFactor = 0.11;
-		
+		// byte[] newBytes = new b
 		System.out.println("Inside createGrayscaleImage");
 		if (color) {
 			for (int i = 0; i < bytes.length; i +=3 ) {
 				double newColor = 0;
 				newColor += bytes[i] *0.3;
 				newColor += bytes[i+1] * 0.59;
-
+				newColor += bytes[i+2] * 0.11;
+				
 			}
 
 		} else {
@@ -427,13 +428,13 @@ public class NLImageServer {
 		}
 		
 		
-
+		System.out.println("New array: ");
 		for (int i = 0; i < bytes.length; ++i) {
 			System.out.println("Byte " + i + ": " + bytes[i]);
 		}
 
 		
-		ImageIcon icon = new ImageIcon(b);
+		// ImageIcon icon = new ImageIcon(bytes);
 		return ret;
 	} 
 
